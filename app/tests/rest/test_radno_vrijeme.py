8971d2f482cdacbdc07174470e57d549eb34bd36
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-16 23:09:56 +0200

    refactor: rearange variables in tests

--- app/tests/rest/test_radno_vrijeme.py
+++ app/tests/rest/test_radno_vrijeme.py
@@ -1,19 +1,15 @@
 from unittest import TestCase
 import requests
 
-zadani_primjeri = ["Kad radite?", "Koje je rano vrijeme?",
+zadani_primjeri = ["Kad radite?", "Koje je radno vrijeme?",
                    "Do kada ste otvoreni danas?"]
 
-modifikacije_zadanih_primjera = [
-    "Od kada do kada radite?", "Koje Vam je rano vrijeme?",
-    "Do kada ste inace otvoreni?"
-]
+
+headers = {"X-API-KEY": "TAJNI_KLJUC"}
 
 
 class RadnoVrijeme(TestCase):
     def test_zadani_primjeri_rade(self):
-        headers = {"X-API-KEY": "TAJNI_KLJUC"}
-
         for n in zadani_primjeri:
             json = {
                 "message": n
@@ -41,3 +37,23 @@ class RadnoVrijeme(TestCase):
 
             assert response.status_code == 200
             assert response.json()["intent"] == "radno_vrijeme"
+
+    # pod typo se misli na malu gresku u jednom slovu
+    def test_primjeri_sa_typoom(self):
+        for primjer in zadani_primjeri:
+            for index in range(len(primjer) - 1):
+                threads = []
+                for lowercase in range(ord("a"), ord("z") + 1):
+                    t1 = threading.Thread(target=assert_replaced_char, args=(
+                        chr(lowercase), index, primjer))
+                    threads.append(t1)
+                    t1.start()
+
+                for uppercase in range(ord("A"), ord("Z") + 1):
+                    t2 = threading.Thread(target=assert_replaced_char, args=(
+                        chr(uppercase), index, primjer))
+                    t2.start()
+                    threads.append(t2)
+
+                for t in threads:
+                    t.join()
