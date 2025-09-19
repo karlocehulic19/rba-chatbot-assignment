1c103f47b9938c01c44162248e3c3c8d4d525437
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-19 15:37:23 +0200

    fix: intent right type property

--- app/tests/rest/utils/intents.py
+++ app/tests/rest/utils/intents.py
@@ -2,7 +2,7 @@ from .examples import Examples
 
 
 class Intent:
-    def __init__(self, intent, canonical_reply, examples):
+    def __init__(self, type, canonical_reply, examples):
         self.type = type
         self.canonical_reply = canonical_reply
         self.examples = Examples()
