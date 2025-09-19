e1c678affea9be9dcf1edff8bab84c5b3f3dabbb
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-19 15:05:57 +0200

    refactor: export intents in separate file

---
+++ app/tests/rest/utils/intents.py
@@ -0,0 +1,9 @@
+from .examples import Examples
+
+
+class Intent:
+    def __init__(self, intent, canonical_reply, examples):
+        self.type = type
+        self.canonical_reply = canonical_reply
+        self.examples = Examples()
+        self.examples.add_example(Examples.DEFAULT_SUBTYPE, examples)
