6bcc380c3b0e168272efc15bef9b7b0e755378c9
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-19 20:43:01 +0200

    add izlozbe custom test

--- app/tests/rest/custom_intent_tests/custom_tests.py
+++ app/tests/rest/custom_intent_tests/custom_tests.py
@@ -1,10 +1,12 @@
 from .test_radno_vrijeme import ExtendedRadnoVrijeme
 from .test_adresa import ExtendedAdresaTest
 from .test_ulaznice import ExtendedUlazniceTest
+from .test_danas_izlozbe import ExtendedDanasIzlozbe
 
 
 custom_tests = [
     ExtendedRadnoVrijeme,
     ExtendedAdresaTest,
-    ExtendedUlazniceTest
+    ExtendedUlazniceTest,
+    ExtendedDanasIzlozbe
 ]
